cd7b4ec505f612c0cd97e43dc0102d3b9259e4b9
Isaiah Edwards <isaiaheedwards@gmail.com>
2020-04-18 16:31:55 -0700

    html and css working

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+//Possible words
+var words =  [
+    "videogames",
+    "playstation",
+    "programmer",
+    "geology",
+    "friendship",
+    "house",
+    "disneywold"
+];
+
+    var answer = "";
+    var maxwrong = 6;
+    var guessed = [];
+    var wins = 0;
